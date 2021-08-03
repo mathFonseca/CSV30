afcd8ace7bc71ef7fef6c93e063897e6f83e7699
Matheus Fonseca <matheus.2016@alunos.utfpr.edu.br>
2021-08-03 18:07:34 -0300

    Refactoring main.py
    Removed unused libraries and refactored comments.
    Added data flow to check current ideas.

--- Projeto#4/main.py
+++ Projeto#4/main.py
@@ -9,10 +9,8 @@
 #===============================================================================
 
 import sys
-import timeit
 import numpy as np
 import cv2
-import math
 
 # Variaveis globais. 
 INPUT_IMAGE = 'Images/150.bmp'
@@ -26,37 +24,55 @@ def main():
 
     # É uma boa prática manter o shape com 3 valores, independente da imagem ser
     # colorida ou não. Também já convertemos para float32.
-    #img = img.astype (np.float32) / 255
+    # img = img.astype (np.float32) / 255
 
-    start_time = timeit.default_timer ()
-
-    # TODO: Binarizar as imgens
+    # Step 1 - Escala de Cinza
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    #Normaliza a imagem (necessario?)
+    # Step 2 - Normalização (Necessário?)
     normalized = np.zeros((800,800))
     normalized = cv2.normalize(gray,normalized,0,255,cv2.NORM_MINMAX)
 
-    #Importante para todas as formas de binarizacao testadas
+    # Step 3 - Gaussian Blur com janela fixa 7x7.
     gaussianBlur = cv2.GaussianBlur(normalized,(7,7),0)
 
-    #Unico que funciona em todas as imagens, mas so temos os contornos
+    # Salvamos imagem antes do processamento.
+    cv2.imwrite('imgBloom.png', gaussianBlur)
+
+    # ----------
+    # Daqui pra baixo, brainstorm.
+
+    # Step 1 - Contar arroz com o que tem.
+    # Step 1.1 - Contornar
+    # Unico que funciona em todas as imagens, mas so temos os contornos
     edges = cv2.Canny(gaussianBlur,100,200)
 
-    #Bom para a maioria, mas morre com 60.bmp e 205.bmp (melhor binarizacao para a 150.bmp)
-    #Talvez seja possivel utilizar em todas se utilizarmos normalizacao local, mas teriamos que criar na mao
+    # Bom para a maioria, mas morre com 60.bmp e 205.bmp (melhor binarizacao para a 150.bmp)
+    # Talvez seja possivel utilizar em todas se utilizarmos normalizacao local, mas teriamos que criar na mao
     otsu = cv2.threshold(gaussianBlur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    #Binariza mas tira um pouco do interior dos graos. Morre com a 150.bmp
+    # Binariza mas tira um pouco do interior dos graos. Morre com a 150.bmp
     adaptative = cv2.adaptiveThreshold(gaussianBlur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,9,4)
 
-    #cv2.imshow('img',img)
-    #cv2.imshow('gray',gray)
-    #cv2.imshow('normalized',normalized)
-    #cv2.imshow('gaussian',gaussianBlur)
-    cv2.imshow('edges',edges)
-    cv2.imshow('adaptative',adaptative)
-    cv2.imshow('Otsu',otsu[1])
+    # Limpar ruídos da imagem Adaptative.
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(2,2))
+    openAdaptative = cv2.morphologyEx(adaptative, cv2.MORPH_OPEN, rectKernel)
+    openCanny = cv2.morphologyEx(edges, cv2.MORPH_OPEN, rectKernel)
+
+    # cv2.imshow('img',img)
+    # cv2.imshow('gray',gray)
+    # cv2.imshow('normalized',normalized)
+    # cv2.imshow('gaussian',gaussianBlur)
+
+    # Salvando pois no Windows não consigo gerar imshow() usando terminal.
+    cv2.imwrite('Edges.png', edges)
+    cv2.imwrite('Adaptative.png', adaptative)
+    cv2.imwrite('openCanny.png', openCanny)
+    cv2.imwrite('Otsu.png', otsu[1])
+    
+    # cv2.imshow('edges',edges)
+    # cv2.imshow('adaptative',adaptative)
+    # cv2.imshow('Otsu',otsu[1])
 
     cv2.waitKey ()
     cv2.destroyAllWindows ()
