a3b36a2dda0a4080d220f6f7f38127b618344602
Matias157 <alexandrematias@alunos.utfpr.edu.br>
2021-08-03 21:47:50 -0300

    Maybe an alternative?

--- Projeto#4/main.py
+++ Projeto#4/main.py
@@ -13,7 +13,7 @@ import numpy as np
 import cv2
 
 # Variaveis globais. 
-INPUT_IMAGE = 'Images/150.bmp'
+INPUT_IMAGE = 'Images/60.bmp'
 
 def main():
     # Abre a imagem.
@@ -46,7 +46,28 @@ def main():
     # Step 1.1 - Contornar
     # Unico que funciona em todas as imagens, mas so temos os contornos
     edges = cv2.Canny(gaussianBlur,100,200)
-
+    contours, _ = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    print("Quantidade de contornos: " + str(len(contours)))
+    num = 0
+    prevx = 0
+    prevy = 0
+    prevw = 0
+    prevh = 0
+    for i, contour in enumerate(contours):
+        x, y, w, h = cv2.boundingRect(contour)
+        area = cv2.contourArea(contour)
+        if((x != prevx) and (y != prevy) and (w != prevw) and (h != prevh)):
+            print("Coordenadas do contorno: Left = " + str(x) + ", Top = " + str(y) + ", Right = " + str(x+w) + ", Botton = " + str(y+h))
+            print("Area do contorno #" + str(i) + ": " + str(area))
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            num += 1
+        else:
+            print("ELSE Coordenadas do contorno: Left = " + str(x) + ", Top = " + str(y) + ", Right = " + str(x+w) + ", Botton = " + str(y+h))
+        prevx = x
+        prevy = y
+        prevw = w
+        prevh = h
+    print(num)
     # Bom para a maioria, mas morre com 60.bmp e 205.bmp (melhor binarizacao para a 150.bmp)
     # Talvez seja possivel utilizar em todas se utilizarmos normalizacao local, mas teriamos que criar na mao
     otsu = cv2.threshold(gaussianBlur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
@@ -59,16 +80,16 @@ def main():
     openAdaptative = cv2.morphologyEx(adaptative, cv2.MORPH_OPEN, rectKernel)
     openCanny = cv2.morphologyEx(edges, cv2.MORPH_OPEN, rectKernel)
 
-    # cv2.imshow('img',img)
+    cv2.imshow('img',img)
     # cv2.imshow('gray',gray)
     # cv2.imshow('normalized',normalized)
     # cv2.imshow('gaussian',gaussianBlur)
 
     # Salvando pois no Windows não consigo gerar imshow() usando terminal.
-    cv2.imwrite('Edges.png', edges)
-    cv2.imwrite('Adaptative.png', adaptative)
-    cv2.imwrite('openCanny.png', openCanny)
-    cv2.imwrite('Otsu.png', otsu[1])
+    # cv2.imwrite('Edges.png', edges)
+    # cv2.imwrite('Adaptative.png', adaptative)
+    # cv2.imwrite('openCanny.png', openCanny)
+    # cv2.imwrite('Otsu.png', otsu[1])
     
     # cv2.imshow('edges',edges)
     # cv2.imshow('adaptative',adaptative)
