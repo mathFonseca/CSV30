7ad1a7cd2887875ec252b1688dbec70867c8b49d
Matheus Fonseca <matheus.2016@alunos.utfpr.edu.br>
2021-08-07 19:09:49 -0300

    C and Sigma Adjusted to minime impact
    Sigma different from 1 causes a lot of disturbance.
    C (Offset of Limiarization process ), have different good values, being inside -50 and -25.
    -25 is the one with least wrong values than -50, but other values can still be checked.
    To run just 1 image at time, swap ALL_IMAGES_TEST to False and use img variable at will. Otherwise will work for all 5 images, but will save none.

--- Projeto#4/main.py
+++ Projeto#4/main.py
@@ -16,6 +16,18 @@ import cv2
 # Variaveis globais. 
 INPUT_IMAGE = 'Images/114.bmp'
 
+IMAGE_1 = 'Images/60.bmp'
+IMAGE_2 = 'Images/82.bmp'
+IMAGE_3 = 'Images/114.bmp'
+IMAGE_4 = 'Images/150.bmp'
+IMAGE_5 = 'Images/205.bmp'
+
+ALL_IMAGES_TEST = True
+
+# Good Candidates to try
+# C = -50 / Sigma = 1
+# C = -25 / Sigma = 1
+
 def inunda (label, img, row, col):
     rows, cols = img.shape
     if(img[row,col] == -1): #verifica se o pixel atual faz parte do blob
@@ -46,12 +58,140 @@ def rotula (img):
         areas.append(pixels)
     return(areas)
 
+def allTest(CTest,SigmaTest = 1):
+    # Abre as imagens.
+    image_01 = cv2.imread(IMAGE_1)
+    if image_01 is None:
+        print ('Erro abrindo a imagem 01.\n')
+        sys.exit ()
+
+    image_02 = cv2.imread(IMAGE_2)
+    if image_02 is None:
+        print ('Erro abrindo a imagem 02.\n')
+        sys.exit ()
+
+    image_03 = cv2.imread(IMAGE_3)
+    if image_03 is None:
+        print ('Erro abrindo a imagem 03.\n')
+        sys.exit ()
+
+    image_04 = cv2.imread(IMAGE_4)
+    if image_04 is None:
+        print ('Erro abrindo a imagem 04.\n')
+        sys.exit ()
+
+    image_05 = cv2.imread(IMAGE_5)
+    if image_05 is None:
+        print ('Erro abrindo a imagem 05.\n')
+        sys.exit ()
+
+    # Escala de Cinza
+    image_01_gray = cv2.cvtColor(image_01, cv2.COLOR_BGR2GRAY)
+    image_02_gray = cv2.cvtColor(image_02, cv2.COLOR_BGR2GRAY)
+    image_03_gray = cv2.cvtColor(image_03, cv2.COLOR_BGR2GRAY)
+    image_04_gray = cv2.cvtColor(image_04, cv2.COLOR_BGR2GRAY)
+    image_05_gray = cv2.cvtColor(image_05, cv2.COLOR_BGR2GRAY)
+
+    # Limiarização Adaptativa
+    C = CTest
+    adaptative_01 = cv2.adaptiveThreshold(image_01_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,C)
+    adaptative_02 = cv2.adaptiveThreshold(image_02_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,C)
+    adaptative_03 = cv2.adaptiveThreshold(image_03_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,C)
+    adaptative_04 = cv2.adaptiveThreshold(image_04_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,C)
+    adaptative_05 = cv2.adaptiveThreshold(image_05_gray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,51,C)
+
+    kernel = np.ones((3,3),np.uint8)
+
+    fechamento_01 = cv2.morphologyEx(adaptative_01,cv2.MORPH_OPEN,kernel)
+    fechamento_02 = cv2.morphologyEx(adaptative_02,cv2.MORPH_OPEN,kernel)
+    fechamento_03 = cv2.morphologyEx(adaptative_03,cv2.MORPH_OPEN,kernel)
+    fechamento_04 = cv2.morphologyEx(adaptative_04,cv2.MORPH_OPEN,kernel)
+    fechamento_05 = cv2.morphologyEx(adaptative_05,cv2.MORPH_OPEN,kernel)
+    
+    fechamento_01 = fechamento_01.astype(np.float32)/255    
+    fechamento_02 = fechamento_02.astype(np.float32)/255    
+    fechamento_03 = fechamento_03.astype(np.float32)/255    
+    fechamento_04 = fechamento_04.astype(np.float32)/255    
+    fechamento_05 = fechamento_05.astype(np.float32)/255
+
+    # Retorna uma lista das areas dos blobs encontrados
+    areas_01 = rotula(fechamento_01)
+    areas_02 = rotula(fechamento_02)
+    areas_03 = rotula(fechamento_03)
+    areas_04 = rotula(fechamento_04)
+    areas_05 = rotula(fechamento_05)
+
+    arr_01 = np.array(areas_01)
+    arr_02 = np.array(areas_02)
+    arr_03 = np.array(areas_03)
+    arr_04 = np.array(areas_04)
+    arr_05 = np.array(areas_05)
+
+    # Cria um histograma das areas encontradas
+    histograma_01 = np.histogram(arr_01, bins='auto')
+    histograma_02 = np.histogram(arr_02, bins='auto')
+    histograma_03 = np.histogram(arr_03, bins='auto')
+    histograma_04 = np.histogram(arr_04, bins='auto')
+    histograma_05 = np.histogram(arr_05, bins='auto')
+
+
+    sigma = SigmaTest 
+    min_area_01 = histograma_01[1][np.argmax(histograma_01[0]) - sigma]
+    max_area_01 = histograma_01[1][np.argmax(histograma_01[0]) + sigma]
+
+    min_area_02 = histograma_02[1][np.argmax(histograma_02[0]) - sigma]
+    max_area_02 = histograma_02[1][np.argmax(histograma_02[0]) + sigma]
+
+    min_area_03 = histograma_03[1][np.argmax(histograma_03[0]) - sigma]
+    max_area_03 = histograma_03[1][np.argmax(histograma_03[0]) + sigma]
+
+    min_area_04 = histograma_04[1][np.argmax(histograma_04[0]) - sigma]
+    max_area_04 = histograma_04[1][np.argmax(histograma_04[0]) + sigma]
+
+    min_area_05 = histograma_05[1][np.argmax(histograma_05[0]) - sigma]
+    max_area_05 = histograma_05[1][np.argmax(histograma_05[0]) + sigma]
+
+    singular_mask_01 = (min_area_01 < arr_01) & (arr_01 <= max_area_01)
+    area_media_01 = np.mean(arr_01[singular_mask_01])
+    n_arroz_01 = int(np.sum(np.round(arr_01/area_media_01)))
+    if( np.abs(60 - n_arroz_01) <= 15  ):
+        print('Numero de arroz Image 01:', n_arroz_01)
+
+    singular_mask_02 = (min_area_02 < arr_02) & (arr_02 <= max_area_02)
+    area_media_02 = np.mean(arr_02[singular_mask_02])
+    n_arroz_02 = int(np.sum(np.round(arr_02/area_media_02)))
+    if( np.abs(82 - n_arroz_02) <= 15  ):
+        print('Numero de arroz Image 02:', n_arroz_02)
+    
+    singular_mask_03 = (min_area_03 < arr_03) & (arr_03 <= max_area_03)
+    area_media_03 = np.mean(arr_03[singular_mask_03])
+    n_arroz_03 = int(np.sum(np.round(arr_03/area_media_03)))
+    if( np.abs(114 - n_arroz_03) <= 15  ):
+        print('Numero de arroz Image 03:', n_arroz_03)
+        
+    singular_mask_04 = (min_area_04 < arr_04) & (arr_04 <= max_area_04)
+    area_media_04 = np.mean(arr_04[singular_mask_04])
+    n_arroz_04 = int(np.sum(np.round(arr_04/area_media_04)))
+    if( np.abs(150 - n_arroz_04) <= 15  ):
+        print('Numero de arroz Image 04:', n_arroz_04)    
+    
+    singular_mask_05 = (min_area_05 < arr_05) & (arr_05 <= max_area_05)
+    area_media_05 = np.mean(arr_05[singular_mask_05])
+    n_arroz_05 = int(np.sum(np.round(arr_05/area_media_05)))
+    if( np.abs(205 - n_arroz_05) <= 15  ):
+        print('Numero de arroz Image 05:', n_arroz_05) 
+    
+
 def main():
-    # Abre a imagem.
+    
     img = cv2.imread (INPUT_IMAGE)
     if img is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
+
+    if ALL_IMAGES_TEST:
+        allTest(-25,1)
+        sys.exit()
 
     # Step 1 - Escala de Cinza
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -93,13 +233,13 @@ def main():
     n_arroz = int(np.sum(np.round(arr/area_media)))
     print('Numero de arroz:', n_arroz)
 
-    cv2.imshow('img',img)
-    cv2.imshow('adaptative',adaptative)
-    cv2.imshow('fechamento',fechamento)
+    # cv2.imshow('img',img)
+    # cv2.imshow('adaptative',adaptative)
+    # cv2.imshow('fechamento',fechamento)
 
     # Salvando pois no Windows não consigo gerar imshow() usando terminal.
-    # cv2.imwrite('Adaptative.png', adaptative)
-    # cv2.imwrite('fechamento.png', fechamento)
+    cv2.imwrite('Adaptative.png', adaptative)
+    cv2.imwrite('fechamento.png', fechamento)
 
     cv2.waitKey ()
     cv2.destroyAllWindows ()
